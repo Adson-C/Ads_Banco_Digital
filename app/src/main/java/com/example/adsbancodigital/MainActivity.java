c55e491c7f5c5812bee020ffd5fc9effa65755a2
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-01-16 22:01:49 -0300

    Criando Layout Activity de Criar Conta

--- app/src/main/java/com/example/adsbancodigital/MainActivity.java
+++ app/src/main/java/com/example/adsbancodigital/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.adsbancodigital;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.adsbancodigital.autenticacao.CadastroActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnapert;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnapert = findViewById(R.id.button);
+
+        btnapert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/adsbancodigital/autenticacao/CadastroActivity.java
@@ -0,0 +1,16 @@
+package com.example.adsbancodigital.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.adsbancodigital.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
